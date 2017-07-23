de86501464b3196a7155c5b226574e07a08b3f73
dragonis <wojtek.sasiela@gmail.com>
2017-07-23 18:51:31 +0200

    refactoring code to services

--- src/main/java/com/example/demo5/ApiController.java
+++ src/main/java/com/example/demo5/ApiController.java
@@ -1,7 +1,6 @@
 package com.example.demo5;
 
-import io.jsonwebtoken.*;
-import org.springframework.beans.factory.annotation.Value;
+import com.example.services.GenerateJWTKTokenService;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,26 +10,16 @@ import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
-import java.security.SignatureException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Date;
 
 @RestController
 public class ApiController {
 
-    private final String secretKey = "secretKey";
+    private GenerateJWTKTokenService generateJWTKTokenService = new GenerateJWTKTokenService();
 
-    @Value("${jwt.liveTokenInSeconds}")
-    private long liveTokenInSeconds;
-
-    /* Maps to all HTTP actions by default (GET,POST,..)*/
     @RequestMapping("/users")
     public @ResponseBody
     String getUsers() {
-        return "{\"users\":[{\"firstname\":\"Richard\", \"lastname\":\"Feynman\"}," +
-                "{\"firstname\":\"Marie\",\"lastname\":\"Curie\"}]}";
+        return generateJWTKTokenService.getUsers();
     }
 
 
@@ -41,33 +30,10 @@ public class ApiController {
             @PathVariable("password") String password
     ) throws UnknownHostException, UnsupportedEncodingException, MalformedURLException, URISyntaxException {
 
+        String url = "http://localhost/download/token/";
+        String generatedToken = generateJWTKTokenService.generateToken(document_id,username,password);
 
-        Date expiration = Date.from(LocalDateTime.now().plusSeconds(1).toInstant(ZoneOffset.UTC));
-
-        long nowMillis = System.currentTimeMillis();
-        Date now = new Date(nowMillis);
-        long ttlMillis = liveTokenInSeconds * 1000; // how many seconds token has been available x secunds = x * 1000 (ms)
-
-        JwtBuilder jwtBuilder = Jwts.builder()
-                .setIssuedAt(Date.from(Instant.now()))
-                .setExpiration(expiration)
-                .claim("document_id", document_id)
-                .claim("username", username)
-                .claim("password", password)
-                .signWith(SignatureAlgorithm.HS256, secretKey.getBytes("UTF-8"));
-
-
-        //if it has been specified, let's add the expiration
-        if (ttlMillis >= 0) {
-            long expMillis = nowMillis + ttlMillis;
-            Date exp = new Date(expMillis);
-            jwtBuilder.setExpiration(exp);
-        }
-        String generatedToken = jwtBuilder.compact();
-
-        generatedToken = generatedToken.replace(".", "/");
-
-        return new ResponseEntity<String>("http://localhost/download/token/"+ generatedToken, null, HttpStatus.OK);
+        return new ResponseEntity<String>(url + generatedToken, null, HttpStatus.OK);
     }
 
     @GetMapping("/download/token/{header}/{payload}/{signature}")
@@ -75,28 +41,8 @@ public class ApiController {
                                   @PathVariable("header") String header,
                                   @PathVariable("payload") String payload,
                                   @PathVariable("signature") String signature)
-            throws UnknownHostException, ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException, IllegalArgumentException, UnsupportedEncodingException {
-        try{
-            String token = header + "." + payload + "." + signature;
-
-
-            Claims claims = Jwts.parser()
-                    .setSigningKey(secretKey.getBytes("UTF-8"))
-                    .parseClaimsJws(token)
-                    .getBody();
-
-            String document_id = claims.get("document_id").toString();
-            String username = claims.get("username").toString();
-            String password = claims.get("password").toString();
-            return "Document_id: " + document_id + " <br> " +
-                    "Username: " + username + ",<br>" +
-                    "Password: " + password;
-
-        }catch(Exception ex)
-        {
-            return ex.getMessage();
-        }
-
+    {
+        return generateJWTKTokenService.receiveToken(header,payload,signature);
     }
 
 
---
+++ src/main/java/com/example/services/GenerateJWTKTokenService.java
@@ -0,0 +1,82 @@
+package com.example.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.io.UnsupportedEncodingException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+public class GenerateJWTKTokenService {
+
+    private final String secretKey = "secretKey";
+
+    /*@Value("${jwt.liveTokenInSeconds}")*/
+    private long liveTokenInSeconds = 10;
+
+    public String generateToken(String document_id, String username, String password) {
+        Date expiration = Date.from(LocalDateTime.now().plusSeconds(1).toInstant(ZoneOffset.UTC));
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        long ttlMillis = liveTokenInSeconds * 1000; // how many seconds token has been available x secunds = x * 1000 (ms)
+
+        JwtBuilder jwtBuilder = null;
+
+        try {
+            jwtBuilder = Jwts.builder()
+                    .setIssuedAt(Date.from(Instant.now()))
+                    .setExpiration(expiration)
+                    .claim("document_id", document_id)
+                    .claim("username", username)
+                    .claim("password", password)
+                    .signWith(SignatureAlgorithm.HS256, secretKey.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
+        //if it has been specified, let's add the expiration
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            jwtBuilder.setExpiration(exp);
+        }
+        String generatedToken = jwtBuilder.compact().replace(".", "/");
+
+        return generatedToken;
+    }
+
+    public String receiveToken(String header, String payload, String signature)
+       {
+
+            try {
+                String token = header + "." + payload + "." + signature;
+
+                Claims claims = Jwts.parser()
+                        .setSigningKey(secretKey.getBytes("UTF-8"))
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                String document_id = claims.get("document_id").toString();
+                String username = claims.get("username").toString();
+                String password = claims.get("password").toString();
+
+                String output = "Document_id: " + document_id + " <br> " +
+                        "Username: " + username + ",<br>" +
+                        "Password: " + password;
+                return output;
+            } catch (Exception ex) {
+                return ex.getMessage();
+            }
+        }
+
+        public String getUsers(){
+            return "{\"users\":[{\"firstname\":\"Richard\", \"lastname\":\"Feynman\"}," +
+                    "{\"firstname\":\"Marie\",\"lastname\":\"Curie\"}]}";
+        }
+    }
